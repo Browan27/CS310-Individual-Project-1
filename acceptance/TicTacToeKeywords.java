a8744bbbacc72b6ab6fc31811fb0f7923139f063
Browan27 <Browan27@yahoo.com>
2016-09-02 12:18:33 -0500

    initialized acceptance

--- acceptance/TicTacToeKeywords.java
+++ acceptance/TicTacToeKeywords.java
@@ -2,12 +2,16 @@ import edu.jsu.mcis.*;
 
 public class TicTacToeKeywords {
 	
+    
+    
 	public void startNewGame() {
-
+        mode = new TicTacToe();
 	}
 	
 	public void markLocation(int row, int col) {
-
+        String m = (turn)? "X" : "O";
+        model.makeMark(m, r, c);
+        turn != turn;
 	}
 	
 	public String getMark(int row, int col) {
